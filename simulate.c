0b402362c75dd4bb472334aeba177500b6c06cb0
JYPark09 <jyp10987@gmail.com>
2021-06-03 17:41:22 +0900

    feat(simulator): fully implement simulator

--- simulate.c
+++ simulate.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
-#define NUM_MEMORY      65536 // maximum number of words in memory.
-#define NUM_REGS        8     // number of machine registers.
+#define NUM_MEMORY 65536 // maximum number of words in memory.
+#define NUM_REGS 8       // number of machine registers.
 #define MAX_LINE_LENGTH 1000
 typedef char string_t[MAX_LINE_LENGTH];
 
@@ -15,14 +15,96 @@ typedef struct
     int numMemory;
 } state_t;
 
-void printState(state_t*);
+/*
+ * Common Codes
+ */
+typedef union
+{
+    unsigned int code;
 
-int main(int argc, char* argv[])
+    struct
+    {
+        unsigned int destReg : 3;
+        unsigned int unused1 : 13;
+        unsigned int regB : 3;
+        unsigned int regA : 3;
+        unsigned int opcode : 3;
+        unsigned int unused0 : 7;
+    } r;
+
+    struct
+    {
+        int offset : 16;
+        unsigned int regB : 3;
+        unsigned int regA : 3;
+        unsigned int opcode : 3;
+        unsigned int unused : 7;
+    } i;
+
+    struct
+    {
+        unsigned int unused1 : 16;
+        unsigned int regB : 3;
+        unsigned int regA : 3;
+        unsigned int opcode : 3;
+        unsigned int unused0 : 7;
+    } j;
+
+    struct
+    {
+        unsigned int unused1 : 22;
+        unsigned int opcode : 3;
+        unsigned int unused0 : 7;
+    } o;
+} inst_t;
+
+enum OpCode
+{
+    OP_ADD = 0b000,
+    OP_NOR = 0b001,
+    OP_LW = 0b010,
+    OP_SW = 0b011,
+    OP_BEQ = 0b100,
+    OP_JALR = 0b101,
+    OP_HALT = 0b110,
+    OP_NOOP = 0b111
+};
+
+/*
+ * Common Codes - End
+ */
+
+// 0 is pc+1, 1 is jump addr
+const char PCMuxTable[] = {
+    [OP_ADD] 0,
+    [OP_NOR] 0,
+    [OP_LW] 0,
+    [OP_SW] 0,
+    [OP_BEQ] 1,
+    [OP_JALR] 1,
+    [OP_HALT] 0,
+    [OP_NOOP] 0,
+};
+
+void printState(state_t *);
+
+void OpADD(const inst_t *inst, state_t *state);
+void OpNOR(const inst_t *inst, state_t *state);
+void OpLW(const inst_t *inst, state_t *state);
+void OpSW(const inst_t *inst, state_t *state);
+void OpBEQ(const inst_t *inst, state_t *state);
+void OpJALR(const inst_t *inst, state_t *state);
+
+int main(int argc, char *argv[])
 {
     string_t line;
     state_t state;
+    inst_t inst;
+    int numOfExecutedInsts = 0;
 
-    FILE* filePtr;
+    FILE *filePtr;
+
+    memset(&state, 0, sizeof state);
 
     if (argc != 2)
     {
@@ -49,10 +131,66 @@ int main(int argc, char* argv[])
         printf("memory[%d]=%d\n", state.numMemory, state.mem[state.numMemory]);
     }
 
+    while (1)
+    {
+        printState(&state);
+
+        // Instruction Fetch & Decode
+        inst.code = state.mem[state.pc];
+
+        // Execute
+        ++numOfExecutedInsts;
+        switch (inst.o.opcode)
+        {
+        case OP_ADD:
+            OpADD(&inst, &state);
+            break;
+
+        case OP_NOR:
+            OpNOR(&inst, &state);
+            break;
+
+        case OP_LW:
+            OpLW(&inst, &state);
+            break;
+
+        case OP_SW:
+            OpSW(&inst, &state);
+            break;
+
+        case OP_BEQ:
+            OpBEQ(&inst, &state);
+            break;
+
+        case OP_JALR:
+            OpJALR(&inst, &state);
+            break;
+
+        case OP_HALT:
+            ++state.pc;
+            goto exit;
+
+        case OP_NOOP:
+            break;
+        default:
+            printf("error: invalid opcode\n");
+            exit(1);
+        }
+
+        if (!PCMuxTable[inst.o.opcode])
+            ++state.pc;
+    }
+
+exit:
+    printf("machine halted\n");
+    printf("total of %d instructions executed\n", numOfExecutedInsts);
+    printf("final state of machine:");
+    printState(&state);
+
     return 0;
 }
 
-void printState(state_t* state)
+void printState(state_t *state)
 {
     int i;
     printf("\n@@@\nstate:\n");
@@ -70,4 +208,50 @@ void printState(state_t* state)
         printf("\t\treg[ %d ] %d\n", i, state->reg[i]);
     }
     printf("end state\n");
+}
+
+void OpADD(const inst_t *inst, state_t *state)
+{
+    unsigned regA = inst->r.regA, regB = inst->r.regB, destReg = inst->r.destReg;
+
+    state->reg[destReg] = state->reg[regA] + state->reg[regB];
+}
+
+void OpNOR(const inst_t *inst, state_t *state)
+{
+    unsigned regA = inst->r.regA, regB = inst->r.regB, destReg = inst->r.destReg;
+
+    state->reg[destReg] = !(state->reg[regA] | state->reg[regB]);
+}
+
+void OpLW(const inst_t *inst, state_t *state)
+{
+    unsigned regA = inst->i.regA, regB = inst->i.regB;
+    int offset = inst->i.offset;
+
+    state->reg[regB] = state->mem[state->reg[regA] + offset];
+}
+
+void OpSW(const inst_t *inst, state_t *state)
+{
+    unsigned regA = inst->i.regA, regB = inst->i.regB;
+    int offset = inst->i.offset;
+
+    state->mem[state->reg[regA] + offset] = state->reg[regB];
+}
+
+void OpBEQ(const inst_t *inst, state_t *state)
+{
+    unsigned regA = inst->i.regA, regB = inst->i.regB;
+    int offset = inst->i.offset;
+
+    state->pc += 1 + offset * (state->reg[regA] == state->reg[regB]);
+}
+
+void OpJALR(const inst_t *inst, state_t *state)
+{
+    unsigned regA = inst->j.regA, regB = inst->j.regB;
+
+    state->reg[regB] = state->pc + 1;
+    state->pc = state->reg[regA];
 }
